2fd86b5f55947d1b9d0ea4f0801ad93c84fadf5a
Anuanushree <anuabi2408@gmail.com>
2024-07-02 22:28:19 +0530

    code update for auto submit chk

--- server.js
+++ server.js
@@ -13,25 +13,7 @@ mongoose
     app.listen(config.PORT, () => {
       console.log(`Server is running on port ${config.PORT}`);
     });
-
-    // Define the time to run the task at the end of the day
-    const scheduleTime = new Date();
-    scheduleTime.setHours(23, 59, 0, 0);
-
-    // Calculate the delay until the scheduled time
-    const now = new Date();
-    let delay = scheduleTime.getTime() - now.getTime();
-
-    // If the scheduled time is in the past, set it for the next day
-    if (delay < 0) {
-      delay += 24 * 60 * 60 * 1000; // Add 24 hours (in milliseconds)
-    }
-
-    // Output the scheduled task information
-    console.log(`Scheduled task will run at ${scheduleTime.toISOString()}`);
-
-    // Execute the task after the calculated delay
-    setTimeout(async function runTask() {
+    const runScheduledTask = async () => {
       try {
         console.log("Running scheduled task...");
 
@@ -44,8 +26,8 @@ mongoose
       }
 
       // Schedule the next run for the next day at the same time
-      setTimeout(runTask, 24 * 60 * 60 * 1000); // Run again after 24 hours
-    }, delay);
+      setTimeout(runScheduledTask, 24 * 60 * 60 * 1000); // Run again after 24 hours
+    };
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);

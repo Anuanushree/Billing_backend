54ab1f6347f841e0401cc23c08e71183295c5fb7
Anuanushree <anuabi2408@gmail.com>
2024-07-13 11:21:08 +0530

    again auto update chk 2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const config = require("./utilis/config");
 const app = require("./App");
 const Formcontroller = require("./controllers/FormDataControllers");
+const cron = require("node-cron");
 
 // Function to run the scheduled task
 const runScheduledTask = async () => {
@@ -9,7 +10,7 @@ const runScheduledTask = async () => {
     console.log("Running scheduled task...");
 
     // Execute the data processing task
-    // await Formcontroller.dd({}, {});
+    await Formcontroller.dd({}, {});
 
     console.log("Scheduled task executed successfully");
   } catch (error) {
@@ -28,38 +29,13 @@ mongoose
       console.log(`Server is running on port ${config.PORT}`);
     });
 
-    // Function to calculate milliseconds until 11:59 PM today
-    const calculateDelayUntilEndOfDay = () => {
-      const now = new Date();
-      const endOfDay = new Date(now);
-      endOfDay.setHours(11);
+    // Schedule the task to run every minute (for testing purposes)
+    cron.schedule("* * * * *", () => {
+      console.log("Scheduled task triggered every minute");
+      runScheduledTask();
+    });
 
-      endOfDay.setMinutes(15);
-      endOfDay.setSeconds(0);
-      endOfDay.setMilliseconds(0);
-
-      let delay = endOfDay.getTime() - now.getTime();
-      if (delay < 0) {
-        // If it's already past 11:59 PM today, calculate delay for tomorrow
-        endOfDay.setDate(endOfDay.getDate() + 1);
-        delay = endOfDay.getTime() - now.getTime();
-      }
-      return delay;
-    };
-
-    // Schedule the task using setTimeout
-    const scheduleTask = () => {
-      const delay = calculateDelayUntilEndOfDay();
-      setTimeout(() => {
-        runScheduledTask();
-        scheduleTask(); // Schedule the next execution
-      }, delay);
-    };
-
-    // Start scheduling the task
-    scheduleTask();
-
-    console.log("Scheduled task will run daily at 11:59 PM");
+    console.log("Scheduled task will run every minute for testing");
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
